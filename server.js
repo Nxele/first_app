e9e1dcd5fe5c16832ec7a079c4e56679d48c53f0
Sizwe Nxele <sizwenduduzow@gmail.com>
2020-04-25 01:04:41 +0200

    added comments on my code

--- server.js
+++ server.js
@@ -3,14 +3,15 @@ var http = require('http');
 var url = require('url');
 var fs = require('fs');
 
+//create the server
 http.createServer(function(req,res){
-    var q = url.parse(req.url,true);
-    var filename = "."+q.pathname;
-    fs.readFile(filename,function(err,data){
-        if(err){
+    var q = url.parse(req.url,true); // get the url passed by the user
+    var filename = "."+q.pathname; // store the user submitted url
+    fs.readFile(filename,function(err,data){ //function for serving requested file
+        if(err){ //if file not found display 404 error message
             res.writeHead(404,{'Content-Type':'text/html'})
             return res.end("404 page not found")
-        }else{
+        }else{ //server the requested file to the user
             res.writeHead(200,{'Content-Type':'text/html'})
             return res.end(data)
         }
