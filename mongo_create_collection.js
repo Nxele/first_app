2d1f59ae99340959d13f0647c43384ec2e42aaa7
Sizwe Nxele <sizwenduduzow@gmail.com>
2020-04-30 23:22:46 +0200

    added code for creating a database and a collection/table

--- mongo_create_collection.js
+++ mongo_create_collection.js
@@ -1,1 +1,12 @@
-var mongo_client = require('mongodb').MongoClient;
+var mongo_client = require('mongodb').MongoClient; //mongoDB module
+var connection_url = "mongodb://localhost:27017"; //connection string
+
+mongo_client.connect(connection_url,function(err,db){  //connect to the db function 
+    if(err) throw erro;
+    var db = db.db('mydb'); //create a database
+    db.createCollection('customer',function(erro,res){ //function for creating a collection/table
+        if(err) throw err;
+        console.log("collection created")
+        db.close()
+    })
+})
